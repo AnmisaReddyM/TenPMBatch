f5ec907ff8c33fd9eb9f5c01a0396747844b8914
AnmisaReddyM <82710101+AnmisaReddyM@users.noreply.github.com>
2021-04-21 18:33:40 +0530

    Add files via upload
    ADDITION

---
+++ add.java
@@ -0,0 +1,35 @@
+public class add {
+    public static void main(String args[])
+    {
+        System.out.println("hai this is Anmisa");
+        int age=24;
+        String citizen = "Canada";
+        if(age <=18 ){
+            System.out.println("anmisa is major");
+            if(citizen == "Indian"){
+                System.out.println("anmisa is eligible to vote");
+            }
+        }
+        else{
+            System.out.println("Anmisa is not eligible to vote");
+        }
+        int a = 24;
+        while (a < 25) {
+            System.out.println("true");
+            a++;}
+        int i;
+
+        System.out.println("Hai this is anmisa");
+        int b=87;
+        do{
+            System.out.println("inside Do");
+        }
+        while(b>897);
+
+        for(i=0; i<5 ; i++)
+        {
+            System.out.println("$");
+            //i=0,1,2,3,4
+        }
+    }
+}
